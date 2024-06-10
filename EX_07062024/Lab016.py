b067392432098001a6084ef99cd2a64052f1449d
anushahv56 <anusha.hv56@gmail.com>
2024-06-10 08:16:08 +0530

    lab0015

---
+++ EX_07062024/Lab016.py
@@ -0,0 +1,14 @@
+a = input("enter the First num")
+b = input("enter the second num")
+print(a+b)
+# Concatination
+
+# convering str to int
+a = int(input("enter the First num"))
+b = int(input("enter the second num"))
+print(type(a+b))
+
+# + is with int-->Sum
+# + is with str-->Concatination
+
+# type cast
