92055610682c9b981b274630fadddc2570ac2587
anushahv56 <anusha.hv56@gmail.com>
2024-06-11 22:38:20 +0530

    Task001 and Task002

---
+++ EX_10062024/Task001.py
@@ -0,0 +1,13 @@
+#Explain the difference between the = operator and the == operator in Python.
+#   = --> assigning values
+#   == -->Comapre the values returns the  bool
+
+
+#What does the ** operator do in Python, and how is it used?
+ #  ** operator acts as a exponential power to a value
+print(2**3)  # retuen 2*2*2=8
+
+#  What does the ^ operator do in Python, and in what context is it commonly used?
+# the ^ operator is the bitwise XOR (exclusive OR) operator.
+#   it’s commonly used in contexts such as encryption, variable swapping, error detection,
+#   and solving specific algorithmic problems where XOR properties are beneficial.
---
+++ EX_10062024/Task002.py
@@ -0,0 +1,13 @@
+#Develope a python file to calculate square and cube of num
+num=2
+square=num**2
+cube=num**3
+print("square of num is",square)
+print("cube of num is",cube)
+
+#Create a program that takes two numbers as input and
+# prints whether the first number is greater than, less than, or equal to the second number.
+n1=float(input("enter the number1"))
+n2=float(input("enter the number2"))
+print(f"{n1}{'greater than'if n1>n2 else'is less than' if n2>n1 else 'is equal to'}{n2}")
+
